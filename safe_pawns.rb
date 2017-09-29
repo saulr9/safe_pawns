28453e26f27c65331fbfd7590af2f5b8aa958bd2
Saul Ramos <saul.ramos.2609@gmail.com>
2017-09-29 18:12:48 -0400

    Max nmro de peones: 8

--- safe_pawns.rb
+++ safe_pawns.rb
@@ -1,11 +1,13 @@
 def safe_pawns(pawns)
 	@coordenadas = pawns.split(",")
 	@rows_cols=[]
-	@pawn_izq=[]
-	@pawn_der=[]
-	@safe_pawns =[]
+	#@pawn_izq=[]
+	#@pawn_der=[]
+	#@safe_pawns =[]
 	@int_safe_pawns = 0
-
+	if (@coordenadas.length<=8)
+		puts "#{@coordenadas.first}"
+	puts "Numero de peones #{@coordenadas.length}"
 	#Convertir columnas a valor numerico
 	@coordenadas.each_with_index{ |v,i|
 		@rows_cols.push([@coordenadas[i][0].ord-64,@coordenadas[i][1].to_i])
@@ -13,8 +15,6 @@ def safe_pawns(pawns)
 	puts "coordenadas: #{@rows_cols}"
 	@rows_cols.each_with_index{ |v,i|
 		puts "indice #{i} - #{@rows_cols[i]}"
-		#puts "valor rows_cols"
-		#puts @rows_cols
 		@pawn_izq=([@rows_cols[i][0].to_i-1, @rows_cols[i][1].to_i-1])
 		puts"valor izq - #{@pawn_izq}"
 		@pawn_der=([@rows_cols[i][0].to_i+1, @rows_cols[i][1].to_i-1])
@@ -28,6 +28,9 @@ def safe_pawns(pawns)
 		puts="======"	
 	}
 	puts @int_safe_pawns > 0 ? "Cantidad de peones seguros: #{@int_safe_pawns}" : "No hay peones seguros"
+	else
+		puts "Introduzca coordenadas validas. Nmro de peones max: 8"
+	end
 end
 puts "Introduzca coordenadas:"
 pawns = gets.to_s
