809fd1fc8ddca02724951e2ed5908cd9678cc03a
Saul Ramos <saul.ramos.2609@gmail.com>
2017-09-29 17:28:00 -0400

    Imprimiendo peones seguros

--- safe_pawns.rb
+++ safe_pawns.rb
@@ -1,21 +1,31 @@
 def safe_pawns(pawns)
 	@coordenadas = pawns.split(",")
 	@rows_cols=[]
+	@pawn_izq=[]
+	@pawn_der=[]
+	@safe_pawns =[]
 	@int_safe_pawns = 0
 
 	#Convertir columnas a valor numerico
 	@coordenadas.each_with_index{ |v,i|
-		@rows_cols.push([@coordenadas[i][0].ord-64,@coordenadas[i][1]])
+		@rows_cols.push([@coordenadas[i][0].ord-64,@coordenadas[i][1].to_i])
 	}
-
+	puts "coordenadas: #{@rows_cols}"
 	@rows_cols.each_with_index{ |v,i|
-		puts "indice #{i}"
-		puts "valor rows_cols"
-		puts @rows_cols
-		puts"valor izq"
-		puts @pawn_izq =([@rows_cols[i][0].to_i-1, @rows_cols[i][1].to_i-1])
-		puts "valor der"
-		puts @pawn_der =([@rows_cols[i][0].to_i+1, @rows_cols[i][1].to_i-1])
+		puts "indice #{i} - #{@rows_cols[i]}"
+		#puts "valor rows_cols"
+		#puts @rows_cols
+		@pawn_izq=([@rows_cols[i][0].to_i-1, @rows_cols[i][1].to_i-1])
+		puts"valor izq - #{@pawn_izq}"
+		@pawn_der=([@rows_cols[i][0].to_i+1, @rows_cols[i][1].to_i-1])
+		puts "valor der - #{@pawn_der}" 
+		if(@rows_cols.include?(@pawn_izq) || @rows_cols.include?(@pawn_der))
+			puts"el valor izquierdo o derecho estan en el arreglo"
+			@int_safe_pawns+=1
+		else
+			puts "no existe"
+		end
+		puts="======"	
 	}
 	puts @int_safe_pawns > 0 ? "Cantidad de peones seguros: #{@int_safe_pawns}" : "No hay peones seguros"
 end
